c784caf1fe8b3e54e00c33f7bf8c7e4d4e9bfaa0
Queenshiba <haruka0111.2a@gmail.com>
2019-10-08 00:01:08 +0900

    inital commit

---
+++ reverse_string.js
@@ -0,0 +1,8 @@
+function reverseString(str) {
+    // Make str into an array
+    // Reverse the array
+    // Return string to an array
+    return str;
+  }
+  
+ console.log(reverseString("hello"));
