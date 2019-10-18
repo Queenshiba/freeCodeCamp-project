b17fb869d533c37e8ddd45430a947c494d86ef3a
Queenshiba <haruka0111.2a@gmail.com>
2019-10-18 16:54:23 +0900

    cleaned up

--- factorialize.js
+++ factorialize.js
@@ -1,6 +1,7 @@
 function factorialize(num) {
     var number = 1;
     for (let i = 0; i <= num; i++) {
+        // if statement number is NOT 0 and i is NOT 0, number * i //
         if (number !== 0 && i !== 0) {
             number = number * i;
         }
