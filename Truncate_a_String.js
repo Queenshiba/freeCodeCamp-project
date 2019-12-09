9b86d767c5f49b0a8f74bea89859510286ad1d6e
Queenshiba <haruka0111.2a@gmail.com>
2019-12-10 00:29:53 +0900

    add comment

--- Truncate_a_String.js
+++ Truncate_a_String.js
@@ -1,12 +1,11 @@
 function truncateString(str, num) {
-    // Clear out that junk in your trunk
+    // if num is same or bigger than the length of str, return str
     if (str.length <= num) {
         return str;
+        // if not, keep a sentence from 0 to a number of num and remove the rest of sentence and add "..."
     } else {
         return str.slice(0, num) + "...";
     }
-
-
 }
 
 console.log(truncateString("A-tisket a-tasket A green and yellow basket", 8));
