3e29931119c06e0d0ceea86d99d68466054b2c7c
Queenshiba <haruka0111.2a@gmail.com>
2019-12-11 13:53:27 +0900

    boo_who

---
+++ boo_who.js
@@ -0,0 +1,10 @@
+function booWho(bool) {
+    // if the type of value is boolean, return true
+    if (typeof bool === "boolean") {
+        return true
+        // if not, return false
+    } else {
+        return false;
+    }
+}
+console.log(booWho(false));
