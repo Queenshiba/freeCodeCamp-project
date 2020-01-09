26554f7e1360f7a984287be504e2a5e121ce8f24
Queenshiba <haruka0111.2a@gmail.com>
2020-01-09 19:40:30 +0900

    diff-two-arrays

--- Intermediate_Algorithm_Scripting/Diff_Two_Arrays.js
+++ Intermediate_Algorithm_Scripting/Diff_Two_Arrays.js
@@ -1,33 +1,26 @@
 function diffArray(arr1, arr2) {
-    var firstArr = []; 
     var newArr = [];
-for ( var i = 0; i < arr1.length; i++){
-    firstArr[arr1[i]] = true;
-}
-for ( var i = 0; i < arr2.length; i++){
-    if (firstArr[arr2[i]]) {
-        delete firstArr[arr2[i]];
-    } else {
-        firstArr[arr2[i]] = true;
+    for (var i = 0; i < arr1.length; i++) {
+        if (arr2.indexOf(arr1[i]) === -1) {
+            newArr.push(arr1[i]);
+        }
     }
-}
-    for ( var k in firstArr) {
-        newArr.push(k)
+    for (var j = 0; j < arr2.length; j++) {
+        if (arr1.indexOf(arr2[j]) === -1) {
+            newArr.push(arr2[j]);
+        }
     }
     // Same, same; but different.
     return newArr;
-  }
-  
-  console.log(diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]));
-  //diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]) //should return [4]
+}
 
-  console.log(["diorite", "andesite", "grass", "dirt", "pink wool", "dead shrub"], ["diorite", "andesite", "grass", "dirt", "dead shrub"]) //should return ["pink wool"].
-//["andesite", "grass", "dirt", "pink wool", "dead shrub"], ["diorite", "andesite", "grass", "dirt", "dead shrub"] 
-//should return ["diorite", "pink wool"].
-//["andesite", "grass", "dirt", "dead shrub"], ["andesite", "grass", "dirt", "dead shrub"] 
-//should return [].
-console.log([1, "calf", 3, "piglet"], [1, "calf", 3, 4] )
-//should return ["piglet", 4].
-//[], ["snuffleupagus", "cookie monster", "elmo"] //should return ["snuffleupagus", "cookie monster", "elmo"].
-//[1, "calf", 3, "piglet"], [7, "filly"] 
-//should return [1, "calf", 3, "piglet", 7, "filly"].
+console.log(diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]));
+//should return [4]
+console.log(diffArray(["diorite", "andesite", "grass", "dirt", "pink wool", "dead shrub"], ["diorite", "andesite", "grass", "dirt", "dead shrub"]))
+// should return ["pink wool"]
+console.log(diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]));
+//should return [4]
+console.log(diffArray(["diorite", "andesite", "grass", "dirt", "pink wool", "dead shrub"], ["diorite", "andesite", "grass", "dirt", "dead shrub"]))
+//should return ["pink wool"].
+console.log(diffArray([1, "calf", 3, "piglet"], [1, "calf", 3, 4]))
+//should return ["piglet", 4]
