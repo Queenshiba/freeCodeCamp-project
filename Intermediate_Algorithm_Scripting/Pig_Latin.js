efeabfbb1a8537abd1699b34b77bedaf6cf253f3
Queenshiba <haruka0111.2a@gmail.com>
2020-02-19 00:04:39 +0900

    added if statement

--- Intermediate_Algorithm_Scripting/Pig_Latin.js
+++ Intermediate_Algorithm_Scripting/Pig_Latin.js
@@ -2,31 +2,36 @@
 //If a word does not contain a vowel, just add "ay" to the end.
 //Input strings are guaranteed to be English words in all lowercase.
 
-
 function translatePigLatin(str) {
     const vowels = ['a', 'e', 'i', 'o', 'u'];
 
     for (let i = 0; i < vowels.length; i++) {
-        if (str[0] == vowels[i]) {
-            return str + "way";
-        } else if (str[0] !== vowels[i]) {
-            const removeFirstCosonant = str.substr(1);
-            return removeFirstCosonant + str[0] + "ay";
-            if  (str[1] !== vowels[i]) {
-            const removeSecondCosonant = str.substr(1, 2);
-            return removeSecondCosonant + str[0] + str[1]  + "ay";
-        } 
-    }
-    //check first letter
+        for (let j = 0; j < str.length; j++) {
+            if (str[0] === vowels[i] || str[0] === "r" && str[1] === "h") {
+                return str + "way";
+            } else if (str[j] !== vowels[i]) {
 
-    //if consonant, keep move to end till find vowel
-    //and add ay to end
-    //if vowel leave it, add way to the end
-    //
+                const removeCosonant = str.substr(str[j]);
+                const restCosnantLength = (str[j].length - str.length);
+                const restCosnant = str.substr(restCosnantLength)
+                const join = removeCosonant + restCosnant;
+
+                console.log( restCosnant)
+
+                return join + "ay"
+
+            }
+
+        }
+        //check first letter
+        //if consonant, keep move to end till find vowel
+        //and add ay to end
+        //if vowel leave it, add way to the end
+        //
     }
 }
 
-console.log(translatePigLatin("consonant"));
+// console.log(translatePigLatin("consonant"));
 // should return "onsonantcay"
 
 // translatePigLatin("california")
@@ -35,11 +40,11 @@ console.log(translatePigLatin("consonant"));
 // //should return "aragraphspay".
 console.log(translatePigLatin("glove"))
 // //should return "oveglay".
-console.log(translatePigLatin("algorithm"))
+// console.log(translatePigLatin("algorithm"))
 // //should return "algorithmway".
 // translatePigLatin("eight")
 // //should return "eightway".
 // translatePigLatin("schwartz")
 // //should return "artzschway".
-// translatePigLatin("rhythm")
+// console.log(translatePigLatin("rhythm"))
 // //should return "rhythmay".
