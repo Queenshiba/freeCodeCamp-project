df34a27765084882e8b2780bd66000289950877a
Queenshiba <haruka0111.2a@gmail.com>
2020-02-20 15:16:39 +0900

    added comment

--- Intermediate_Algorithm_Scripting/Pig_Latin.js
+++ Intermediate_Algorithm_Scripting/Pig_Latin.js
@@ -1,72 +1,38 @@
-//If a word begins with a vowel you just add "way" to the end.
-//If a word does not contain a vowel, just add "ay" to the end.
-//Input strings are guaranteed to be English words in all lowercase.
+// THE RULE OF PIG LATIN
+// --- If a word begins with a vowel you just add "way" to the end.
+// --- If a word does not contain a vowel, just add "ay" to the end.
+// --- Input strings are guaranteed to be English words in all lowercase.
 
 function translatePigLatin(str) {
-    const regex = /[aeiuo]/g;
-    if (str[0].match(regex) || str.match(regex) === null) {
+    const vowels = /[aeiuo]/g;
+    if (str[0].match(vowels)) {
         return str + "way"
-    } else if (str.match(regex) === null) {
-        return str + "way"
+    } else if (str.match(vowels) === null) {
+        return str + "ay"
     } else {
-        const test = str.indexOf(str.match(regex)[0]) // index of first vowel
-        // console.log(test)
-        const removeCosonants = str.substr(test)
-        const restStr = str.substr(0, test)
-        
+        const firstVowel = str.match(vowels)[0];
+        // Or you can use this instead
+        //const [firstVowel] = str.match(vowels);
+        const indexOftheFirstVowel = str.indexOf(firstVowel) // index of first vowel
+        const removeCosonants = str.substr(indexOftheFirstVowel) //
+        const restStr = str.substr(0, indexOftheFirstVowel);
 
         return removeCosonants + restStr + "ay"
     }
-
-    //check first letter
-    //if consonant, keep move to end till find vowel
-    //and add ay to end
-    //if vowel leave it, add way to the end
-
-
-
-
-
 }
-
-// function translatePigLatin(str) {
-//     const vowels = ['a', 'e', 'i', 'o', 'u'];
-//     const arr = []
-//     for (let i = 0; i < vowels.length; i++) {
-//         for (let j = 0; j < str.length; j++) {
-//             if (str[0] === vowels[i] || str[0] === "r" && str[1] === "h") {
-//                 return str + "way";
-//             } else if (str[j] !== vowels[i]) {
-//                 // arr.push(str[j])
-//                 const restCosnantLength = (str[j].length - str.length);
-//                 const removed = str.substr(restCosnantLength);
-//                 // const restCosnant = str.substr(restCosnantLength)
-//                 // const join = removeCosonant + restCosnant;
-//                 arr.push(removed)
-//                 // console.log(restCosnantLength)  
-//             }
-//             return arr
-//         }
-//     }
-//     //check first letter
-//     //if consonant, keep move to end till find vowel
-//     //and add ay to end
-//     //if vowel leave it, add way to the end
-// }
 
 
 console.log(translatePigLatin("consonant"));
 // should return "onsonantcay"
-
-// translatePigLatin("california")
+console.log(translatePigLatin("california"))
 // //should return "aliforniacay".
-// translatePigLatin("paragraphs")
+console.log(translatePigLatin("paragraphs"))
 // //should return "aragraphspay".
 console.log(translatePigLatin("glove"))
 // //should return "oveglay".
 console.log(translatePigLatin("algorithm"))
 // //should return "algorithmway".
-// translatePigLatin("eight")
+console.log(translatePigLatin("eight"))
 // //should return "eightway".
 console.log(translatePigLatin("schwartz"))
 // //should return "artzschway".
