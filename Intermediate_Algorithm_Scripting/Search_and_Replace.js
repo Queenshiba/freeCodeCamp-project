49420ccf11d7ce14543950a67e99d337202b33f5
Queenshiba <haruka0111.2a@gmail.com>
2020-03-01 21:36:59 +0900

    added comment

--- Intermediate_Algorithm_Scripting/Search_and_Replace.js
+++ Intermediate_Algorithm_Scripting/Search_and_Replace.js
@@ -1,21 +1,15 @@
 function myReplace(str, before, after) {
-    //find the second argument in the first
-    //check the first letter of the second arg is uppercase or not
-    //if the uppercase change the third arg
-    //replace the third arg to where the second one was
-
-    const upperCase = /[A-Z]/g;
-    // const search = str.match(before)
+    const upperCase = /[A-Z]/g; // all uppercase letters named as uppercase
 
 
-    if (before[0].match(upperCase)) {
-        const upperCasedAfter = after[0].toUpperCase()
-        const restOfAfter = after.slice(1)
-        const newAfter = upperCasedAfter + restOfAfter
-        const replace = str.replace(before, newAfter)
+    if (before[0].match(upperCase)) { // if the first letter of before has uppercase
+        const upperCasedAfter = after[0].toUpperCase(); // also make the first letter of after uppercase
+        const restOfAfter = after.slice(1); // remove the first letter of after
+        const newAfter = upperCasedAfter + restOfAfter; // combine the uppercased first letter and after without the first letter 
+        const replace = str.replace(before, newAfter) // replace before to newAfter 
         return replace
-    } else {
-        return str.replace(before, after)
+    } else { // if the first letter of before is not uppercase
+        return str.replace(before, after) // simply replace it
     }
 }
 
@@ -24,13 +18,20 @@ function myReplace(str, before, after) {
 console.log(myReplace("A quick brown fox jumped over the lazy dog", "jumped", "leaped"));
 
 
-// myReplace("Let us go to the store", "store", "mall")
-// //should return "Let us go to the mall".
-// myReplace("He is Sleeping on the couch", "Sleeping", "sitting")
-// //should return "He is Sitting on the couch".
-// myReplace("This has a spellngi error", "spellngi", "spelling")
-// //should return "This has a spelling error".
+myReplace("Let us go to the store", "store", "mall")
+//should return "Let us go to the mall".
+myReplace("He is Sleeping on the couch", "Sleeping", "sitting")
+//should return "He is Sitting on the couch".
+myReplace("This has a spellngi error", "spellngi", "spelling")
+//should return "This has a spelling error".
 console.log(myReplace("His name is Tom", "Tom", "john"))
-// //should return "His name is John".
-// myReplace("Let us get back to more Coding", "Coding", "algorithms")
-//should return "Let us get back to more Algorithms".
+//should return "His name is John".
+myReplace("Let us get back to more Coding", "Coding", "algorithms")
+// should return "Let us get back to more Algorithms".
+
+
+// My note
+    //find the second argument in the first
+    //check the first letter of the second arg is uppercase or not
+    //if the uppercase change the third arg
+    //replace the third arg to where the second one was
