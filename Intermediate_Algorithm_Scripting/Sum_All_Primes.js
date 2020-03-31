19358d706f73f9abcd3d117f29a21a5a23125a2f
Queenshiba <haruka0111.2a@gmail.com>
2020-03-31 20:41:03 +0900

    finished sum all prime nums

--- Intermediate_Algorithm_Scripting/Sum_All_Primes.js
+++ Intermediate_Algorithm_Scripting/Sum_All_Primes.js
@@ -3,7 +3,6 @@ function sumPrimes(num) {
         if (num === 1) {
             return false
         }
-
         for (let i = 2; i < num; i++) {
             if (num % i === 0) {
                 return false
@@ -12,18 +11,26 @@ function sumPrimes(num) {
         return true
     }
     let arr = []
-    for (let i = 1; i < num; i++) {
+    for (let i = 1; i <= num; i++) {
         arr.push(i)
     }
-    return arr
+    let primeNums = []
+    for (let j = 0; j < arr.length; j++) {
+        if (isPrime(arr[j])) {
+            primeNums.push(arr[j])
+        }
 
+    }
+    let sum = primeNums.reduce((a, b) => a + b, 0)
+return sum
 
 }
 
 
-console.log(isPrime(10));
-console.log(isPrime(13));
-// console.log(sumPrimes(10));
+// console.log(isPrime(10));
+// console.log(isPrime(13));
+console.log(sumPrimes(10));
+console.log(sumPrimes(13));
 // should return 17.
 // sumPrimes(977)
 // should return 73156.
