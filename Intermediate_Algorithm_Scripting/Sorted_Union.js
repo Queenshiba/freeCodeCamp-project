019b232ed9510eaa7cc30d85e4dae2259610b247
Queenshiba <haruka0111.2a@gmail.com>
2020-04-08 22:05:59 +0900

    finished sorted union

--- Intermediate_Algorithm_Scripting/Sorted_Union.js
+++ Intermediate_Algorithm_Scripting/Sorted_Union.js
@@ -1,10 +1,20 @@
-function uniteUnique(arr) {
-    return arr;
+function uniteUnique(...arr) {
+  let test = []
+for (let i = 0; i < arr.length; i++) {
+arr[i].forEach(element => {
+  if (!test.includes(element))
+  test.push(element)
+});
+  
+}
+
+
+    return test;
   }
   
   uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1]);
 
 
 console.log(uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1])) // should return [1, 3, 2, 5, 4].
-console.log(uniteUnique([1, 2, 3], [5, 2, 1])) // should return [1, 2, 3, 5].
+// console.log(uniteUnique([1, 2, 3], [5, 2, 1])) // should return [1, 2, 3, 5].
 console.log(uniteUnique([1, 2, 3], [5, 2, 1, 4], [2, 1], [6, 7, 8])) // should return [1, 2, 3, 5, 4, 6, 7, 8].
