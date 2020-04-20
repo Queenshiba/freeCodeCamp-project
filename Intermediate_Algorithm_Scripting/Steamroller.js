fcedb06266be45eccb5cc71b7dc12be4177cf37a
Queenshiba <haruka0111.2a@gmail.com>
2020-04-20 14:51:02 +0900

    completed binary agents

--- Intermediate_Algorithm_Scripting/Steamroller.js
+++ Intermediate_Algorithm_Scripting/Steamroller.js
@@ -1,13 +1,22 @@
 function steamrollArray(arr) {
-    let test = []
+    let arrStr = arr.toString()
+    let replacedStr = arrStr.replace(",,", ",");
+    let spletReplacedStr = replacedStr.split(",")
+    console.log(spletReplacedStr)
 
-return test
+    // spletReplacedStr.map(item => {
+    //     if (typeof item === 'object') {
+    //         return {}
+    //     }
+    // }
+    // )
+console.log(spletReplacedStr[1])
 }
 
-console.log(steamrollArray([1, [2], [3, [[4]]]]));
 
-console.log(steamrollArray([[["a"]], [["b"]]])) // should return ["a", "b"].
+// console.log(steamrollArray([1, [2], [3, [[4]]]]));
+// console.log(steamrollArray([[["a"]], [["b"]]])) // should return ["a", "b"].
 // steamrollArray([1, [2], [3, [[4]]]]) // should return [1, 2, 3, 4].
 // console.log(steamrollArray([1, [], [3, [[4]]]])) // should return [1, 3, 4].
-// console.log(steamrollArray([1, {}, [3, [[4]]]])) // should return [1, {}, 3, 4].
+console.log(steamrollArray([1, {}, [3, [[4]]]])) // should return [1, {}, 3, 4].
 // Your solution should not use the Array.prototype.flat() or Array.prototype.flatMap() methods.
