771ce7dda9dd48302db1feb3e217f9e433e03ff3
Queenshiba <haruka0111.2a@gmail.com>
2020-05-10 22:03:11 +0900

    add comment

--- Intermediate_Algorithm_Scripting/Steamroller.js
+++ Intermediate_Algorithm_Scripting/Steamroller.js
@@ -1,28 +1,33 @@
 function steamrollArray(arr) {
+    // make arr string
     let arrStr = arr.toString()
+    // if arr has ",,", make them ","
     let replacedStr = arrStr.replace(",,", ",");
+    // then splet them by ","
     let spletReplacedStr = replacedStr.split(",")
-    console.log(spletReplacedStr)
-    // let array = []
-   let result = spletReplacedStr.map(item => {
+
+    // console.log(spletReplacedStr)
+
+    // use map function
+    let result = spletReplacedStr.map(item => {
+        // if any item is "[object Object]", return {}
         if (item == "[object Object]") {
             return {}
-        } 
-        else if (isNaN(item)) {
-            // if not a number (string)
+        }
+
+        else {
+
             return item;
-        } 
-        // else {
-        //     return parseInt(item); // if a number in a string, convert it
-        // }
+        }
+
     })
-return result
+    return result
 }
 
 
-// console.log(steamrollArray([1, [2], [3, [[4]]]]));
-// console.log(steamrollArray([[["a"]], [["b"]]])) // should return ["a", "b"].
-// steamrollArray([1, [2], [3, [[4]]]]) // should return [1, 2, 3, 4].
-// console.log(steamrollArray([1, [], [3, [[4]]]])) // should return [1, 3, 4].
+console.log(steamrollArray([1, [2], [3, [[4]]]]));
+console.log(steamrollArray([[["a"]], [["b"]]])) // should return ["a", "b"].
+steamrollArray([1, [2], [3, [[4]]]]) // should return [1, 2, 3, 4].
+console.log(steamrollArray([1, [], [3, [[4]]]])) // should return [1, 3, 4].
 console.log(steamrollArray([1, {}, [3, [[4]]]])) // should return [1, {}, 3, 4].
 // Your solution should not use the Array.prototype.flat() or Array.prototype.flatMap() methods.
