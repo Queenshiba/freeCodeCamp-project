4e8cad766013d06f8843392b1bb8d50b66e51b6b
Queenshiba <haruka0111.2a@gmail.com>
2020-05-17 00:06:05 +0900

    used map

--- Intermediate_Algorithm_Scripting/Map_the_Debris.js
+++ Intermediate_Algorithm_Scripting/Map_the_Debris.js
@@ -2,31 +2,15 @@ function orbitalPeriod(arr) {
     var GM = 398600.4418;
     var earthRadius = 6367.4447;
 
-    const answer = arr.map(({ name, avgAlt }) => {
+    return arr.map(({ name, avgAlt }) => {
         let insideRoot = Math.pow(earthRadius + avgAlt, 3) / GM;
         let removedRoot = Math.sqrt(insideRoot)
         let sum = 2 * Math.PI * removedRoot;
-        let rounded = Math.round(sum)
-        console.log(rounded)
+        let orbitalPeriod = Math.round(sum)
+        // console.log(rounded)
+        return { name, orbitalPeriod}
     })
 
-
-    // for (let i = 0; i < arr.length; i++) {
-    //     let aveAlt = Object.values(arr[i])[1]
-    //     // add earthRadius and aveAlt
-    //     let sumOfEarthradiusAndAvealt = earthRadius + aveAlt
-    //     // console.log(rounded)
-    //     let cubedSum = Math.pow(sumOfEarthradiusAndAvealt, 3)
-    //     // cubedSum devides GM
-    //     let devidedByGM = cubedSum / GM
-    //     let roundDevidedByGM = Math.round(devidedByGM)
-    //     let removedRoot = Math.sqrt(roundDevidedByGM)
-    //     let leftpart = 2 * Math.PI
-    //     let sum = leftpart * removedRoot
-    //     let integerSum = Math.ceil(sum)
-    //     return integerSum
-    // }
-    
 }
 
 
