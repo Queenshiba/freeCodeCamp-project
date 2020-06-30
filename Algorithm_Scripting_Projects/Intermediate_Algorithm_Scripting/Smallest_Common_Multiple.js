8b4b6b389e423bf00b7e7edf354f6623761caa4b
Queenshiba <haruka0111.2a@gmail.com>
2020-06-30 22:55:54 +0900

    add new code to check the rangeArr

--- Algorithm_Scripting_Projects/Intermediate_Algorithm_Scripting/Smallest_Common_Multiple.js
+++ Algorithm_Scripting_Projects/Intermediate_Algorithm_Scripting/Smallest_Common_Multiple.js
@@ -4,47 +4,30 @@ function smallestCommons(arr) {
     const min = arr[0] > arr[1] ? arr[1] : arr[0]
     const max = arr[0] > arr[1] ? arr[0] : arr[1]
 
-    console.log(min, max)
-let num = max
+    // console.log(min, max)
+    // let's declear max as num
+    
+    let rangeArr = []
+    for (let i = min; i <= max; i++) {
+        rangeArr.push(i)
 
-    // let array = []
-    // let sum = min
-    // for (let i = min; i <= max; i++) {
-    //     array.push(i)
-    // }
-    // console.log(array)
-
-    // for (let k = 0; k < array.length; k++) {
-    //     let sumArr = array.reduce( (a,b) => a * b, 1 )
-       
-    //     let divide = sumArr / array[k]
-    //     console.log(divide)
-    // } 
- 
-for (let i = max - 1; i >= min; i--) {
-    // Each time the solution checks (i.e. sol%i===0) it won't be necessary
-    // to increment 'max' to our solution and restart the loop
-    if (num % i) {
-        num += max;
-      i = max;
     }
-  }
-  return num;
+
+let answer = null;
+    for (let j = 1; answer === null; j++) {
+       console.log(max * j)
+      
+        if (j === 7) {
+            answer = true
+       }
+    }
+    // console.log(rangeArr)
+
 }
 
-
-
-
-
-
-// console.log(range)
-
-
-
-
 // smallestCommons([1, 5]);
-// console.log(smallestCommons([1, 5])) // should return 60.
-console.log(smallestCommons([5, 1]))  // should return 60.
-console.log(smallestCommons([2, 10]))  // should return 2520.
-console.log(smallestCommons([1, 13]))  // should return 360360.
-console.log(smallestCommons([23, 18]))  // should return 6056820.  
+console.log(smallestCommons([1, 5])) // should return 60.
+// console.log(smallestCommons([5, 1]))  // should return 60.
+// console.log(smallestCommons([2, 10]))  // should return 2520.
+// console.log(smallestCommons([1, 13]))  // should return 360360.
+// console.log(smallestCommons([23, 18]))  // should return 6056820.  
