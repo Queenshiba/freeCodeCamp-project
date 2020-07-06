42084451d5f4f21c23afdc86e51253ecade22ba6
Queenshiba <haruka0111.2a@gmail.com>
2020-07-07 00:54:06 +0900

    solved issue

--- Algorithm_Scripting_Projects/JavaScript_Algorithms_and_Data_Structures_Projects/PalindromeChecker.js
+++ Algorithm_Scripting_Projects/JavaScript_Algorithms_and_Data_Structures_Projects/PalindromeChecker.js
@@ -4,34 +4,32 @@ function palindrome(str) {
     let lowercasesRemoved = removed.toLowerCase()
     let splitRemoved = lowercasesRemoved.split('')
 
-    // console.log('splitRemoved', splitRemoved[0])
-    console.log(splitRemoved)
-
     let reverse = [...splitRemoved].reverse()
-    // let reverseLower = reverse.toLowerCase()
-    // console.log('reverse', reverse[0])
-    console.log(reverse)
 
+    let booleanChecker;
     for (let i = 0; i < splitRemoved.length; i++) {
-        if (splitRemoved[i] !== reverse[i]) {
+        if (booleanChecker === false) {
+            break
+        }
+        if (splitRemoved[i] === reverse[i]) {
             // console.log("yes")
-            return true
+            booleanChecker = true
         } else {
-            return false
+            boolenChecker = false
             // console.log("no")
         }
-        
     }
+    return booleanChecker
 }
-// palindromse("eye");
+console.log(palindrome("eye"));
 
-// console.log(palindrome("_eye")) // should return true.
+console.log(palindrome("_eye")) // should return true.
 // console.log(palindrome("race car")) // should return true.
 // console.log(palindrome("not a palindrome")) // should return false.
 //   console.log(palindrome("A man, a plan, a canal. Panama")) // should return true.
 //   console.log(palindrome("never odd or even")) // should return true.
 // console.log(palindrome("nope")) // should return false.
-  console.log(palindrome("almostomla")) // should return false.
+console.log(palindrome("almostomla")) // should return false.
 //   console.log(palindrome("My age is 0, 0 si ega ym.")) // should return true.
 //   console.log(palindrome("1 eye for of 1 eye.")) // should return false.
 //   console.log(palindrome("0_0 (: /-\ :) 0-0")) // should return true.
