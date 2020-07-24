41383accaac19ef3ef484f0ccabe793d0d32d23a
Queenshiba <haruka0111.2a@gmail.com>
2020-07-24 23:26:45 +0900

    reverse arrays and add division

--- Algorithm_Scripting_Projects/JavaScript_Algorithms_and_Data_Structures_Projects/Roman_Numeral_Converter.js
+++ Algorithm_Scripting_Projects/JavaScript_Algorithms_and_Data_Structures_Projects/Roman_Numeral_Converter.js
@@ -1,7 +1,7 @@
 
 function convertToRoman(num) {
-    let main = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '40', '50', '60', '100', '400', '500', '900', '1000']
-    let roman = ['I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX', 'X', 'XL', 'L', 'XC', 'C', 'CD', 'D', 'CM', 'M']
+    let main = ["1000", "900", "500", "400", "100", "60", "50", "40", "10", "9", "8", "7", "6", "5", "4", "3", "2", "1"]
+    let roman = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "VIII", "VII", "VI", "V", "IV", "III", "II", "I"]
 
     let stringSplitedNum = num.toString().split('')
     let devidedNum = []
@@ -13,13 +13,18 @@ function convertToRoman(num) {
     let ansewer = []
     for (let j = 0; j < main.length; j++) {
 
-if (devidedNum.includes(main[j])) {
-     ansewer.push(roman[j])   
-}
+        if (devidedNum.includes(main[j])) {
+            ansewer.push(roman[j])
+        } else {
+            ansewer.unshift(roman[j].repeat(devidedNum[0] / main[j]))
+            
+        }
+
     }
+
     return ansewer
 }
-
+console.log(convertToRoman(20))// should return "II".
 
 // console.log(convertToRoman(2))// should return "II".
 // console.log(convertToRoman(3))//  should return "III".
@@ -28,14 +33,14 @@ if (devidedNum.includes(main[j])) {
 // console.log(convertToRoman(9))//  should return "IX".
 // console.log(convertToRoman(12)) //  should return "XII".
 // console.log(convertToRoman(16)) //  should return "XVI".
-console.log(convertToRoman(29)) //  should return "XXIX".
+// console.log(convertToRoman(29)) //  should return "XXIX".
 // console.log(convertToRoman(44)) //  should return "XLIV".
-console.log(convertToRoman(45)) //  should return "XLV"
+// console.log(convertToRoman(45)) //  should return "XLV"
 // console.log(convertToRoman(68)) //  should return "LXVIII"
 // console.log(convertToRoman(83)) //  should return "LXXXIII"
-console.log(convertToRoman(97)) //  should return "XCVII"
+// console.log(convertToRoman(97)) //  should return "XCVII"
 // console.log(convertToRoman(99)) //  should return "XCIX"
-console.log(convertToRoman(400)) //  should return "CD"
+// console.log(convertToRoman(400)) //  should return "CD"
 // console.log(convertToRoman(500)) //  should return "D"
 // console.log(convertToRoman(501)) //  should return "DI"
 // console.log(convertToRoman(649)) //  should return "DCXLIX"
@@ -46,4 +51,4 @@ console.log(convertToRoman(400)) //  should return "CD"
 // console.log(convertToRoman(1006)) //  should return "MVI"
 // console.log(convertToRoman(1023)) //  should return "MXXIII"
 // console.log(convertToRoman(2014)) //  should return "MMXIV"
-console.log(convertToRoman(3999)) //  should return "MMMCMXCIX"
+// console.log(convertToRoman(3999)) //  should return "MMMCMXCIX"
