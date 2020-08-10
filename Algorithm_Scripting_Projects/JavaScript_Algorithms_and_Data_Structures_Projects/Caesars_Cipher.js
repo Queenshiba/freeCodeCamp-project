57b5fa4683fe688759e525e7fbbf0a23867db258
Queenshiba <haruka0111.2a@gmail.com>
2020-08-10 23:09:29 +0900

    solved caesars cipher

--- Algorithm_Scripting_Projects/JavaScript_Algorithms_and_Data_Structures_Projects/Caesars_Cipher.js
+++ Algorithm_Scripting_Projects/JavaScript_Algorithms_and_Data_Structures_Projects/Caesars_Cipher.js
@@ -1,25 +1,30 @@
 function rot13(str) {
-    console.log(str)
+    // Convert letters to integers between 0 and 65535 representing the UTF-16 code unit
     let original = []
     for (let i = 0; i < str.length; i++) {
         original.push(str[i].charCodeAt())
     }
+    // Sort the integers 3 patterns if A(65) - M(77), add 13
+    // if N(78) - Z(90), minus 13 
+    // if something else like !(33), ?(63), .(46) or space(32), return the integers as it is.
     let added = []
     original.forEach((e) => {
-        if (e < 78 && e > 90 ) {
-            added.push(e + 13)
-        } else if (e < 65 && e > 77 ) {
+        if (e >= 78) {
             added.push(e - 13)
+        } else if (e >= 65) {
+            added.push(e + 13)
+        } else if (e < 64){
+            added.push(e)
         }
-
     })
-    console.log(added)
+
+    // Lastly convert the converted integers into strings created from the specified sequence of UTF-16 code units
     let result = []
     for (let j = 0; j < added.length; j++) {
         result.push(String.fromCharCode(added[j]))
     }
-    return result
-
+    // join all letters with no commas
+    return result.join('')
 }
 
 
@@ -27,7 +32,7 @@ console.log(rot13("SERR PBQR PNZC"))
 // should decode to FREE CODE CAMP
 console.log(rot13("SERR CVMMN!"))
 // // should decode to FREE PIZZA!
-// console.log(rot13("SERR YBIR?"))
+console.log(rot13("SERR YBIR?"))
 // // should decode to FREE LOVE?
-// console.log(rot13("GUR DHVPX OEBJA SBK WHZCF BIRE GUR YNML QBT."))
+console.log(rot13("GUR DHVPX OEBJA SBK WHZCF BIRE GUR YNML QBT."))
  // should decode to THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG.
