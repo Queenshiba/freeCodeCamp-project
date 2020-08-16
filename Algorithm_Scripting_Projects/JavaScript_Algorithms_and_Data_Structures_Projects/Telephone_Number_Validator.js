83da6140ed638570fe8915bed0eae0f0918048fa
Queenshiba <haruka0111.2a@gmail.com>
2020-08-16 17:52:46 +0900

    add new regex

--- Algorithm_Scripting_Projects/JavaScript_Algorithms_and_Data_Structures_Projects/Telephone_Number_Validator.js
+++ Algorithm_Scripting_Projects/JavaScript_Algorithms_and_Data_Structures_Projects/Telephone_Number_Validator.js
@@ -1,39 +1,43 @@
 function telephoneCheck(str) {
-  let replaced = str.replace(/\s+/g, "");
-  const reg = ^\([0-9]{3}\)[0-9]{3}-[0-9]{4}$
-console.log(replaced)
+
+  const reg = /^\(?\(\d{1}[1]?)$/
+
+
   // if (str.length < 9) {
   //   return false
-  // }
-    // return true
+  // } 
+
+    return reg.test(str)
+  
+
   
 }
-  console.log(telephoneCheck("555-555-5555"));
-  
-  console.log(telephoneCheck("555-555-5555")) //should return a boolean.
-  console.log(telephoneCheck("1 555-555-5555")) // should return true.
-  console.log(telephoneCheck("1 (555) 555-5555")) // should return true.
-  console.log(telephoneCheck("5555555555")) // should return true.
-  console.log(telephoneCheck("555-555-5555")) // should return true.
-  console.log(telephoneCheck("(555)555-5555")) // should return true.
-  console.log(telephoneCheck("1(555)555-5555")) // should return true.
-  console.log(telephoneCheck("555-5555")) // should return false.
-  console.log(telephoneCheck("5555555")) // should return false.
-  console.log(telephoneCheck("1 555)555-5555")) // should return false.
-  console.log(telephoneCheck("1 555 555 5555")) // should return true.
-  console.log(telephoneCheck("1 456 789 4444")) // should return true.
-  console.log(telephoneCheck("123**&!!asdf#")) // should return false.
-  // console.log(telephoneCheck("55555555")) // should return false.
-  // console.log(telephoneCheck("(6054756961)")) // should return false
-  // console.log(telephoneCheck("2 (757) 622-7382")) // should return false.
-  // console.log(telephoneCheck("0 (757) 622-7382")) // should return false.
-  // console.log(telephoneCheck("-1 (757) 622-7382")) // should return false
-  // console.log(telephoneCheck("2 757 622-7382")) // should return false.
-  // console.log(telephoneCheck("10 (757) 622-7382")) // should return false.
-  // console.log(telephoneCheck("27576227382")) // should return false.
-  // console.log(telephoneCheck("(275)76227382")) // should return false.
-  // console.log(telephoneCheck("2(757)6227382")) // should return false.
-  // console.log(telephoneCheck("2(757)622-7382")) // should return false.
-  // console.log(telephoneCheck("555)-555-5555")) // should return false.
-  // console.log(telephoneCheck("(555-555-5555")) // should return false.
-  console.log(telephoneCheck("(555)5(55?)-5555")) //should return false.
+console.log(telephoneCheck("555-555-5555"));
+
+console.log(telephoneCheck("555-555-5555")) //should return a boolean.
+console.log(telephoneCheck("1 555-555-5555")) // should return true.
+console.log(telephoneCheck("1 (555) 555-5555")) // should return true.
+console.log(telephoneCheck("5555555555")) // should return true.
+console.log(telephoneCheck("555-555-5555")) // should return true.
+console.log(telephoneCheck("(555)555-5555")) // should return true.
+console.log(telephoneCheck("1(555)555-5555")) // should return true.
+console.log(telephoneCheck("555-5555")) // should return false.
+console.log(telephoneCheck("5555555")) // should return false.
+console.log(telephoneCheck("1 555)555-5555")) // should return false.
+console.log(telephoneCheck("1 555 555 5555")) // should return true.
+console.log(telephoneCheck("1 456 789 4444")) // should return true.
+console.log(telephoneCheck("123**&!!asdf#")) // should return false.
+// console.log(telephoneCheck("55555555")) // should return false.
+// console.log(telephoneCheck("(6054756961)")) // should return false
+// console.log(telephoneCheck("2 (757) 622-7382")) // should return false.
+// console.log(telephoneCheck("0 (757) 622-7382")) // should return false.
+// console.log(telephoneCheck("-1 (757) 622-7382")) // should return false
+// console.log(telephoneCheck("2 757 622-7382")) // should return false.
+// console.log(telephoneCheck("10 (757) 622-7382")) // should return false.
+// console.log(telephoneCheck("27576227382")) // should return false.
+// console.log(telephoneCheck("(275)76227382")) // should return false.
+// console.log(telephoneCheck("2(757)6227382")) // should return false.
+// console.log(telephoneCheck("2(757)622-7382")) // should return false.
+// console.log(telephoneCheck("555)-555-5555")) // should return false.
+// console.log(telephoneCheck("(555-555-5555")) // should return false.
+console.log(telephoneCheck("(555)5(55?)-5555")) //should return false.
