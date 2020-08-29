b68ac05a4e1aec211a65dc1ab4b2b3cb16866388
Queenshiba <haruka0111.2a@gmail.com>
2020-08-29 19:46:14 +0900

    add change

--- Algorithm_Scripting_Projects/JavaScript_Algorithms_and_Data_Structures_Projects/Cash_Register.js
+++ Algorithm_Scripting_Projects/JavaScript_Algorithms_and_Data_Structures_Projects/Cash_Register.js
@@ -1,6 +1,22 @@
 function checkCashRegister(price, cash, cid) {
-    var change;
-    return change;
+    let change = cash - price
+let list = [
+    ["PENNY", 0.01],
+    ["NICKEL", 0.05],
+    ["DIME", 0.1],
+    ["QUARTER", 0.25],
+    ["ONE", 1.00],
+    ["FIVE", 5.00],
+    ["TEN", 10.00],
+    ["TWENTY", 20.00],
+    ["ONE HUNDRED", 100.00]
+  ]
+
+  console.log(list[2])
+
+
+
+
 }
 
 checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]);
