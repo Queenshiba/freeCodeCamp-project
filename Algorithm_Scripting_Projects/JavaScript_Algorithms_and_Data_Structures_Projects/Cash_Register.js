4199342b4ba30880e0c2464efbc1ba4f8a95cde1
Queenshiba <haruka0111.2a@gmail.com>
2020-11-09 17:37:53 +0900

    create object but math problem

--- Algorithm_Scripting_Projects/JavaScript_Algorithms_and_Data_Structures_Projects/Cash_Register.js
+++ Algorithm_Scripting_Projects/JavaScript_Algorithms_and_Data_Structures_Projects/Cash_Register.js
@@ -8,8 +8,9 @@ function checkCashRegister(price, cash, cid) {
 
   let change = cash - price
   let adjustedChange = change.toFixed(2)
-  console.log(adjustedChange)
-console.log('change is', adjustedChange)
+  let numAdjustedChange = parseFloat(adjustedChange)
+  console.log(numAdjustedChange)
+console.log(typeof numAdjustedChange)
   // keep numbers of cid values in order to sum them up later
   let cidValues = []
   cid.forEach(item => {
@@ -36,26 +37,31 @@ console.log('change is', adjustedChange)
     "ONE HUNDRED": 100.00
   }
 
+  // compare change to coins BUT NEED TO CALCULATE THE DITAIL OF CHANGE!!!!
+let changeFromCid = {};
+let objKeys = Object.keys(listOfCoin)
+let objValues = Object.values(listOfCoin)
+for (let i =0; i < objKeys.length; i++){
+  if (objValues[i] === numAdjustedChange) {
 
-  for (let k in listOfCoin) {
-    if (!listOfCoin.hasOwnProperty(k)) continue;
-    if (listOfCoin[k] === adjustedSum) {
-      /* yaay! an orange! */
-    }
-  }
-
+    let theObjKey = objKeys[i]
+    let theObjValue = objValues[i]
+    changeFromCid[theObjKey] = theObjValue
+  } 
+}
+console.log('changeFromCid', changeFromCid)
 
   // Prepere for showing the results
   let NewResult = {}
-if (adjustedSum === adjustedChange) {
+if (adjustedSum === numAdjustedChange) {
   NewResult.status = 'ClOSED'
   NewResult.change = cid
-} else if (adjustedSum < adjustedChange){
+} else if (adjustedSum < numAdjustedChange){
   NewResult.status = 'INSUFFICIENT_FUNDS'
   NewResult.change = []
-} else if (adjustedSum > adjustedChange){
+} else if (adjustedSum > numAdjustedChange){
   NewResult.status = 'OPEN'
-  NewResult.change = 'change'
+  NewResult.change = changeFromCid
 }
   
 console.log(NewResult)
@@ -81,6 +87,6 @@ console.log(checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DI
 
 // console.log(checkCashRegister(3.26, 100, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]) ) 
 // should return {status: "OPEN", change: [["TWENTY", 60], ["TEN", 20], ["FIVE", 15], ["ONE", 1], ["QUARTER", 0.5], ["DIME", 0.2], ["PENNY", 0.04]]}.
-console.log(checkCashRegister(19.5, 20, [["PENNY", 0.01], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]) ) // should return {status: "INSUFFICIENT_FUNDS", change: []}.
+// console.log(checkCashRegister(19.5, 20, [["PENNY", 0.01], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]) ) // should return {status: "INSUFFICIENT_FUNDS", change: []}.
 // console.log(checkCashRegister(19.5, 20, [["PENNY", 0.01], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 1], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]) ) // should return {status: "INSUFFICIENT_FUNDS", change: []}.
-console.log(checkCashRegister(19.5, 20, [["PENNY", 0.5], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]) ) // should return {status: "CLOSED", change: [["PENNY", 0.5], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]}.
+// console.log(checkCashRegister(19.5, 20, [["PENNY", 0.5], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]) ) // should return {status: "CLOSED", change: [["PENNY", 0.5], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]}.
