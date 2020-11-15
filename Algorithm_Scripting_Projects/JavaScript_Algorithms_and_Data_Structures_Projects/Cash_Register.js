4a997a6166e6edc307dc6168b42bb2dde83839db
Queenshiba <haruka0111.2a@gmail.com>
2020-11-15 23:49:35 +0900

    solving a returning change

--- Algorithm_Scripting_Projects/JavaScript_Algorithms_and_Data_Structures_Projects/Cash_Register.js
+++ Algorithm_Scripting_Projects/JavaScript_Algorithms_and_Data_Structures_Projects/Cash_Register.js
@@ -10,93 +10,104 @@ function checkCashRegister(price, cash, cid) {
   let adjustedChange = change.toFixed(2)
   let numAdjustedChange = parseFloat(adjustedChange)
   console.log(numAdjustedChange)
-console.log(typeof numAdjustedChange)
+  console.log(typeof numAdjustedChange)
   // keep numbers of cid values in order to sum them up later
   let cidValues = []
   cid.forEach(item => {
     cidValues.push(item[1])
   })
-// Sum up the numbers in Cid 
+  // Sum up the numbers in Cid 
   for (i = 0; i < cidValues.length; i++) {
     let sumOfCidValues = cidValues.reduce((a, b) => a + b)
-    adjustedSum= sumOfCidValues.toFixed(2)
-  
+    adjustedSum = sumOfCidValues.toFixed(2)
+
   }
   console.log("how much left in Cid", adjustedSum)
 
   // the change and which coin 
   let listOfCoin = {
-    "PENNY": 0.01,
-    "NICKEL": 0.05,
-    "DIME": 0.1,
-    "QUARTER": 0.25,
-    "ONE": 1.00,
-    "FIVE": 5.00,
-    "TEN": 10.00,
+    "ONE HUNDRED": 100.00,
     "TWENTY": 20.00,
-    "ONE HUNDRED": 100.00
+    "TEN": 10.00,
+    "FIVE": 5.00,
+    "ONE": 1.00,
+    "QUARTER": 0.25,
+    "DIME": 0.1,
+    "NICKEL": 0.05,
+    "PENNY": 0.01,
   }
 
 // Add some function for calculate for change
 
-let change = 7.00
+let change = 113.50
 let values = Object.values(listOfCoin)
 let arr =[]
 for (let i =0; i < values.length; i++){
-  if (change > values[i]){
-    arr.push(change - values[i])
+  gap = change - values[i]
+ 
+  if (gap >  values[i]){
+    let loopgap = gap - values[i]
+  } if (gap = 0) {
+    break
   }
+  // console.log(gap)
+  // if (change > values[i]){
+  //   arr.push(change - values[i])
+  // }
 }
-console.log(arr)
+console.log(gap)
+  console.log(arr)
+
+
 
 
 
   // compare change to coins BUT NEED TO CALCULATE THE DITAIL OF CHANGE!!!!
-let changeFromCid = {};
-let objKeys = Object.keys(listOfCoin)
-let objValues = Object.values(listOfCoin)
-for (let i =0; i < objKeys.length; i++){
-  if (objValues[i] === numAdjustedChange) {
+  let changeFromCid = {};
+  let objKeys = Object.keys(listOfCoin)
+  let objValues = Object.values(listOfCoin)
+  for (let i = 0; i < objKeys.length; i++) {
+    if (objValues[i] === numAdjustedChange) {
 
-    let theObjKey = objKeys[i]
-    let theObjValue = objValues[i]
-    changeFromCid[theObjKey] = theObjValue
-  } 
-}
-console.log('changeFromCid', changeFromCid)
+      let theObjKey = objKeys[i]
+      let theObjValue = objValues[i]
+      changeFromCid[theObjKey] = theObjValue
+    }
+  }
+  console.log('changeFromCid', changeFromCid)
 
   // Prepere for showing the results
   let NewResult = {}
-if (adjustedSum === numAdjustedChange) {
-  NewResult.status = 'ClOSED'
-  NewResult.change = cid
-} else if (adjustedSum < numAdjustedChange){
-  NewResult.status = 'INSUFFICIENT_FUNDS'
-  NewResult.change = []
-} else if (adjustedSum > numAdjustedChange){
-  NewResult.status = 'OPEN'
-  NewResult.change = changeFromCid
-}
-  
-console.log(NewResult)
-
-
-
-    // {status: "INSUFFICIENT_FUNDS", change: []}
-    // {status: "CLOSED", change: [...]}
-    // {status: "OPEN", change: [...]}
-    // console.log()
-
-    // console.log(Math.round(sums))
-
+  if (adjustedSum === numAdjustedChange) {
+    NewResult.status = 'ClOSED'
+    NewResult.change = cid
+  } else if (adjustedSum < numAdjustedChange) {
+    NewResult.status = 'INSUFFICIENT_FUNDS'
+    NewResult.change = []
+  } else if (adjustedSum > numAdjustedChange) {
+    NewResult.status = 'OPEN'
+    NewResult.change = changeFromCid
   }
+
+  console.log(NewResult)
+
+
+
+  // {status: "INSUFFICIENT_FUNDS", change: []}
+  // {status: "CLOSED", change: [...]}
+  // {status: "OPEN", change: [...]}
+  // console.log()
+
+  // console.log(Math.round(sums))
+
+}
 
 
 
 
 // console.log(checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]])) 
 // // should return an object.
-console.log(checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]])) 
+console.log(checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]))
 //  should return {status: "OPEN", change: [["QUARTER", 0.5]]}.
 
 // console.log(checkCashRegister(3.26, 100, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]) ) 
