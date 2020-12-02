08f56ca335111583299e3e3d5744243ac13786d8
Queenshiba <haruka0111.2a@gmail.com>
2020-12-02 20:03:29 +0900

    adjust decimal

--- Algorithm_Scripting_Projects/JavaScript_Algorithms_and_Data_Structures_Projects/Cash_Register.js
+++ Algorithm_Scripting_Projects/JavaScript_Algorithms_and_Data_Structures_Projects/Cash_Register.js
@@ -1,7 +1,3 @@
-function checkCashRegister(price, cash, cid) {
-  // Check how much the change is
-  // Check how much is in the register
-
   // Check if it's possible to make the amount of the change from the register
   // if the amount is smaller or not exact the change, return "INSUFFICIENT_FUNDS"
   // if you can return the change, return it with sorted high to low
@@ -33,30 +29,32 @@ function checkCashRegister(price, cash, cid) {
 
     // Compare the total of cid and the change to return
     // if the total of cid is LESS THAN the change to return
-    let detailOfChange = []
+    const detailOfChange = []
     if (totalCid < changeToReturn) {
-      return { status: "INSUFFICIENT_FUNDS", change: [] }
+      return { status: "INSUFFICIENT_FUNDS", change: detailOfChange }
       // if the change is exact same
-    } else if (totalCid === changeToReturn) {
+    } else if (totalCid === changeToReturn.toFixed(2)) {
       return { status: "CLOSED", change: cid }
     } else {
       for (let elem of cid) {
         let temp = [elem[0], 0];
+        let tempToFixed ;
         while (changeToReturn >= list[elem[0]] && elem[1] > 0) {
           temp[1] += list[elem[0]]
           elem[1] -= list[elem[0]]
-          changeToReturn -= list[elem[1]];
+          changeToReturn -= list[elem[0]];
           changeToReturn = changeToReturn.toFixed(2);
 
         }
 
         if (temp[1] > 0) {
-          detailOfChange.push(temp)
+          tempToFixed = temp[1].toFixed(2)
+          detailOfChange.push(tempToFixed)
 
         }
-
+console.log("temp", tempToFixed)
       }
-      console.log("temp", temp)
+      
     }
 
     if (changeToReturn > 0) {
@@ -65,7 +63,7 @@ function checkCashRegister(price, cash, cid) {
 
     return { status: "OPEN", change: detailOfChange }
   }
-}
+
 
   // Prepere for showing the results
   // let NewResult = {}
@@ -90,10 +88,10 @@ function checkCashRegister(price, cash, cid) {
 
 // console.log(checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]])) 
 // // should return an object.
-// console.log(checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]))
+console.log(checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]))
 //  should return {status: "OPEN", change: [["QUARTER", 0.5]]}.
 
-console.log(checkCashRegister(3.26, 100, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]))
+// console.log(checkCashRegister(3.26, 100, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]))
 // should return {status: "OPEN", change: [["TWENTY", 60], ["TEN", 20], ["FIVE", 15], ["ONE", 1], ["QUARTER", 0.5], ["DIME", 0.2], ["PENNY", 0.04]]}.
 // console.log(checkCashRegister(19.5, 20, [["PENNY", 0.01], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]) ) // should return {status: "INSUFFICIENT_FUNDS", change: []}.
 // console.log(checkCashRegister(19.5, 20, [["PENNY", 0.01], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 1], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]) ) // should return {status: "INSUFFICIENT_FUNDS", change: []}.
